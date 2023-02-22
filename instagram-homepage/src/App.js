907d8fe5b11367fd07e5c4d10530b31a304d76a4
stephaniehutama <stephaniehutama@gmail.com>
2023-02-22 11:30:01 +0700

    setup

---
+++ instagram-homepage/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HomeScreen from "./views/HomeScreen";
+
+function App() {
+  return (
+    <div className="App">
+      <HomeScreen />
+    </div>
+  );
+}
+
+export default App;
